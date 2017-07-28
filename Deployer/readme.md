9b207e11284ac1441f10e1c17f4c23bf1527288d
ZVanoZ <dio.zim.work@gmail.com>
2017-07-28 12:44:24 +0300

    Рабочий пример Deployer4/S00

---
+++ Deployer/readme.md
@@ -0,0 +1,7 @@
+# Эксперименты с Deployer
+
+## Ссылки
+
+https://deployer.org/
+https://github.com/deployphp/deployer
+https://habrahabr.ru/post/302442/
---
+++ Deployer/v4php5/Sample00/deploy.php
@@ -0,0 +1,50 @@
+<?php
+namespace Deployer;
+require 'recipe/zend_framework.php';
+
+// Configuration
+
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+
+set('repository', 'https://github.com/ZVanoZ/sander.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+
+add('writable_dirs', []);
+
+// Servers
+
+server('test1', '127.0.0.1')
+    //->user('ivan')
+    //->identityFile()
+    //->pty(true)
+    ->set('deploy_path', __DIR__ . '/test1.deploy_path')
+;
+server('test2', '127.0.0.1')
+    //->user('ivan')
+    //->identityFile()
+    //->pty(true)
+    ->set('deploy_path', __DIR__ . '/test2.deploy_path')
+;
+ 
+
+// Tasks
+task('deploy:vendors', function () {
+    writeln("Переопределили deploy:vendors...");
+})->desc('Описание для deploy:vendors');
+
+desc('Restart PHP-FPM service');
+task('php-fpm:restart', function () {
+    writeln("Перезапуск PHP...");
+    // The user must have rights for restart service
+    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+//    run('sudo systemctl restart php-fpm.service');
+});
+
+
+after('deploy:symlink', 'php-fpm:restart');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
